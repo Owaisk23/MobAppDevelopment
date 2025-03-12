32f3cbe1709d4df5136c9776f3e633e7d95c1853
Owais Ahmed <owaisahmed@aptech.com>
2025-03-12 13:40:51 +0500

    mob app development on flutter

---
+++ dart/api/lib/api.dart
@@ -0,0 +1,25 @@
+import 'package:http/http.dart' as HTTP;
+import 'dart:convert' as CONVERT;
+
+void main() async {
+  final API = Uri.parse(
+    'https://67d13a6e825945773eb37cfc.mockapi.io/dev/studentInfo',
+  );
+  HTTP.get(API).then((response) {
+    if (response.statusCode == 200) {
+      final data = CONVERT.jsonDecode(response.body);
+      print(data);
+      List<dynamic> studentInfo = data;
+      for (var student in studentInfo) {
+        print('!!==============================!!');
+        print('Name: ${student['studentName']}');
+        print('Age: ${student['studentAge']}');
+        print('Avatar: ${student['avatar']}');
+      }
+    }
+  });
+
+  // final Response = await HTTP.get(API);
+
+  // print(Response.body);
+}
