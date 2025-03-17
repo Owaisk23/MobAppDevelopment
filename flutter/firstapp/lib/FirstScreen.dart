006005a5d2ac678aa398fd2a9dba46f33436c9a3
Owais Ahmed <owaisahmed@aptech.com>
2025-03-17 15:36:53 +0500

    added first home widget

---
+++ flutter/firstapp/lib/FirstScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({ Key? key }) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: Text('First Screen'),
+        centerTitle: true,
+      ),
+      body: Text('This is the first screen'),
+      
+    );
+  }
+}
