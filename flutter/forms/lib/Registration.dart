a5e2126d39c419768fbc86947397a2266f7689f7
owaisk23 <owais1566@gmail.com>
2025-03-19 13:40:12 +0500

    added registration form

--- flutter/forms/lib/Registration.dart
+++ flutter/forms/lib/Registration.dart
@@ -10,13 +10,66 @@ class Registration extends StatefulWidget {
 class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController UserName = new TextEditingController();
+    TextEditingController UserEmail = new TextEditingController();
+    TextEditingController UserPassword = new TextEditingController();    
+
+    void formSubmit() {
+      print('Name: ${UserName.text}');
+      print('Email: ${UserEmail.text}');
+      print('Password: ${UserPassword.text}');
+    }
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Registration Screen'),
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
       ),
-      body: Text("Registration Screen"),
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+
+          child: Column(
+            children: [
+              TextField(
+                controller: UserName,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: 'Enter your name',
+                  labelText: 'Name',
+                ),
+              ),
+              SizedBox(height: 5,),
+              TextField(
+                controller: UserEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(height: 5,),
+              TextField(
+                controller: UserPassword,
+                autocorrect: false,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(height: 5,),
+              ElevatedButton(
+                onPressed: (formSubmit),
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
