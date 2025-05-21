d3448f38f9687e62d7c44e306c9219a43d5243e1
owaisk23 <owais1566@gmail.com>
2025-05-21 17:04:22 +0500

    added img crud

--- flutter/firstapp/lib/main.dart
+++ flutter/firstapp/lib/main.dart
@@ -1,19 +1,27 @@
 // import 'package:firstapp/DarazScreen.dart';
 // import 'package:firstapp/FirstScreen.dart';
 // import 'package:firstapp/details.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firstapp/BIllCalculator.dart';
+import 'package:firstapp/addproduct.dart';
+import 'package:firstapp/firebase_options.dart';
+import 'package:firstapp/productFetch.dart';
 // import 'package:firstapp/product.dart';
 // import 'package:firstapp/test.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  
-  runApp(
+void main()async {
+
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BIllCalculator()
+      home: Addproduct()
     ),
   );
 }
+
 
 
---
+++ flutter/firstapp/lib/productFetch.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProductFetch extends StatefulWidget {
+  const ProductFetch({ Key? key }) : super(key: key);
+
+  @override
+  _ProductFetchState createState() => _ProductFetchState();
+}
+
+class _ProductFetchState extends State<ProductFetch> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
